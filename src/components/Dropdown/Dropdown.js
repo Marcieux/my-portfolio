971fc0a9816dd4bdc9643e855392a9914f4c1d26
Marcieux <marcj.doria@gmail.com>
2023-08-20 16:07:20 +0800

    update manifest json and dropdown

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FaTimes } from "react-icons/fa";
 import styled from "@emotion/styled";
 import { Link as ScrollLink } from "react-scroll";
+import PDF from '../../assets/resume/DORIA_RESUME.pdf'
 
 const SiderBar = styled.div`
   background: #151418;
@@ -86,11 +87,11 @@ function Dropdown({ isOpen, toggle }) {
       <NavBtn onClick={toggle}>
         <a
           className="btn PrimaryBtn"
-          href="https://linkedin.com/in/pedro-sales-muniz"
+          href= {PDF}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Resume
+          Download Resume
         </a>
       </NavBtn>
     </SiderBar>
